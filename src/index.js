d068eae8e9eda113b74e8986dad910266d216fda
Arua <aruadecarvalho@gmail.com>
2022-06-06 19:39:57 -0300

    Implementação de carregar mais fotos conforme da scroll, alteração no css da SearchPage, funcionalidades para limpar a pagina e botão de pesquisa

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SearchPage from "./components/SearchPage";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-    {/* <SearchPage /> */}
+    {/* <App /> */}
+    <SearchPage />
   </React.StrictMode>
 );
