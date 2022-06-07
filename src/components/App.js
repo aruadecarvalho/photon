577641c1b604c5e09c61472d6c264112e5be3b66
Arua <aruadecarvalho@gmail.com>
2022-06-07 15:52:02 -0300

    React router e reorganização das pastas

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ErrorPage from "./Pages/ErrorPage";
-import Home from "./Pages/Home";
-import SearchPage from "./Pages/SearchPage";
+import Home from "./Pages/Home.js";
+import SearchPage from "./Pages/SearchPage.js";
 
 function App() {
   return (
@@ -14,5 +14,4 @@ function App() {
     </Router>
   );
 }
-
 export default App;
